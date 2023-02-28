df800187126449c8cb224aa947447586b4a222a2
hhhbini <121172127+hhhbini@users.noreply.github.com>
2023-02-28 14:38:29 +0900

    complete

--- src/index.js
+++ src/index.js
@@ -8,26 +8,28 @@ function handleClick(event) {
   const rangeNumber = range.value;
   const selectNumber = select.value;
   const randomNumber = Math.ceil(Math.random() * parseInt(range.value));
-  /*if (selectNumber == None && randomNumber == None) {
-    prompt("값을 모두 입력해 주세요!");
-  } else {
-  }*/
-  event.preventDefault();
-  hideGame.innerHTML =
-    "You chose: " + selectNumber + " ,the machine chose: " + randomNumber + ".";
 
-  if (selectNumber == randomNumber) {
-    hideResult.innerHTML = "You won!";
+  event.preventDefault();
+  if (rangeNumber < 0) {
+    alert("0 이상의 범위를 입력해 주세요!");
+  } else if (rangeNumber == "" || selectNumber == "") {
+    alert("값을 모두 입력해 주세요!");
+  } else if (selectNumber > rangeNumber) {
+    alert("범위 내의 값를 선택해 주세요!");
   } else {
-    hideResult.innerHTML = "You lost!";
+    hideGame.innerHTML =
+      "You chose: " +
+      selectNumber +
+      " ,the machine chose: " +
+      randomNumber +
+      ".";
+
+    if (selectNumber == randomNumber) {
+      hideResult.innerHTML = "You won!";
+    } else {
+      hideResult.innerHTML = "You lost!";
+    }
   }
 }
 
 playBtn.addEventListener("click", handleClick);
-
-/* 
-1. 범위값을 입력한다. (음수 포함될 수 없음)
-2. 숫자를 선택한다. (범위값 내)
-3. Play를 누르면 게임 진행 텍스트가 나온다. (유저가 숫자를 입력 안하면 진행 안됨) - innerHTML
-+) 실시간으로 범위 값을 업데이트해야 합니다. - preventDefault() 
- */
